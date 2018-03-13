392d86d09a688341e68056d0e1f15a110ebcc3a4
liuzheng712 <liuzheng712@gmail.com>
2018-03-13 18:52:31 +0800

    feat: add delete function for ali api

--- jms_storage/ali.py
+++ jms_storage/ali.py
@@ -31,6 +31,13 @@ class ali:
     def has_file(self, remote_path):
         return self.client.object_exists(remote_path)
 
+    def delete_file(self, remote_path):
+        try:
+            self.client.delete_object(remote_path)
+            return True
+        except:
+            return False
+
     def download_file(self, remote_path, locale_path):
         try:
             self.client.get_object_to_file(remote_path, locale_path)
