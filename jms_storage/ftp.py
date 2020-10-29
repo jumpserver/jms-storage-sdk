5f0919e311c0bced0c205418e9ae7d2d45f701b1
ibuler <ibuler@qq.com>
2020-10-29 15:50:52 +0800

    fix: 优化一下sftp

--- jms_storage/ftp.py
+++ jms_storage/ftp.py
@@ -2,7 +2,7 @@
 #
 
 import os
-from ftplib import FTP
+from ftplib import FTP, error_perm
 from .base import ObjectStorage
 
 
@@ -13,27 +13,32 @@ class FTPStorage(ObjectStorage):
         self.port = config.get("PORT", 21)
         self.username = config.get("USERNAME", None)
         self.password = config.get("PASSWORD", None)
+        self.pasv = config.get("PASV", False)
+        self.dir = config.get("DIR", ".")
         self.client = FTP()
         self.client.encoding = 'utf-8'
-        self.client.set_pasv(True)
+        self.client.set_pasv(self.pasv)
         self.connect()
+        self.client.cwd(self.dir)
+        self.pwd = self.client.pwd()
 
     def connect(self, timeout=-999, source_address=None):
         self.client.connect(self.host, self.port, timeout, source_address)
         self.client.login(self.username, self.password)
 
     def upload(self, src, target):
-        file_name = target.rsplit('/', 1)[-1] if '/' in target else target
-        if self.exists(target):
-            raise Exception('File exist error(%s)' % target)
+        target_dir = os.path.dirname(target)
+        exist = self.check_dir_exist(target_dir)
+        if not exist:
+            ok = self.mkdir(target_dir)
+            if not ok:
+                raise PermissionError('Dir create error: %s' % target)
         try:
             with open(src, 'rb') as f:
-                self.client.storbinary('STOR '+file_name, f)
+                self.client.storbinary('STOR '+target, f)
             return True, None
         except Exception as e:
             return False, e
-        finally:
-            self.client.close()
 
     def download(self, src, target):
         try:
@@ -43,39 +48,55 @@ class FTPStorage(ObjectStorage):
             return True, None
         except Exception as e:
             return False, e
-        finally:
-            self.client.close()
 
     def delete(self, path):
-        file_name = path.rsplit('/', 1)[-1] if '/' in path else path
         if not self.exists(path):
-            raise Exception('File not exist error(%s)' % path)
+            raise FileNotFoundError('File not exist error(%s)' % path)
         try:
-            self.client.delete(file_name)
+            self.client.delete(path)
             return True, None
         except Exception as e:
             return False, e
 
-    def exists(self, path):
-        if '/' in path:
-            remote_dir, file_name = path.rsplit('/', 1)[0], path.rsplit('/', 1)[1]
-        else:
-            remote_dir, file_name = '/', path
+    def check_dir_exist(self, d):
+        pwd = self.client.pwd()
         try:
-            self.client.cwd(remote_dir)
-            if file_name in self.client.nlst():
-                return True
-            return False
-        except:
-            self.mkdir(remote_dir)
+            self.client.cwd(d)
+            self.client.cwd(pwd)
+            return True
+        except error_perm:
             return False
 
-    def mkdir(self, dir):
+    def mkdir(self, dirs):
         # 创建多级目录，ftplib不支持一次创建多级目录
-        dir_list = dir.split('/')
-        for d in dir_list:
-            try:
-                self.client.mkd(d)
-            except:
-                pass
-            self.client.cwd(d)
+        dir_list = dirs.split('/')
+        pwd = self.client.pwd()
+        try:
+            for d in dir_list:
+                if not d or d in ['.']:
+                    continue
+                # 尝试切换目录
+                try:
+                    self.client.cwd(d)
+                    continue
+                except:
+                    pass
+                # 切换失败创建这个目录，再切换
+                try:
+                    self.client.mkd(d)
+                    self.client.cwd(d)
+                except:
+                    return False
+            return True
+        finally:
+            self.client.cwd(pwd)
+
+    def exists(self, target):
+        try:
+            self.client.size(target)
+            return True
+        except:
+            return False
+
+    def close(self):
+        self.client.close()
